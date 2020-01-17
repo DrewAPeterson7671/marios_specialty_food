44bb92bf90dcf72115f82e67387b3b67086e8f0f
Student <example@example.com>
2020-01-17 09:08:20 -0800

    Add products table

--- db/migrate/20200117164638_create_products.rb
+++ db/migrate/20200117164638_create_products.rb
@@ -1,6 +1,11 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
+      t.column(:name, :string)
+      t.column(:price, :decimal, precision: 7, scale: 2)
+      t.column(:country, :string)
+
+      t.timestamps()
     end
   end
 end
